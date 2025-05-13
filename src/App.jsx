2fa67518325e74e017fe90ee15fe4b20f3cc1069
nikakakbexs <kakabadzenikanika@gmail.com>
2025-05-13 11:01:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+   <div>
+    <Header/>
+    <h1>hello world</h1>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="w-full bg-[#191919] flex justify-between p-5 ">
+        <div className=" flex h-100% items-center gap-10  ">
+          <button className=" cursor-pointer lg:hidden">
+            <img src="./assets/menu.svg" alt="" />
+          </button>
+
+          <button className="cursor-pointer  ">
+            <img src="./assets/audiophile.svg" alt="" />
+          </button>
+        </div>
+        <nav className="lg:flex text-[#FFFFFF] gap-8 hidden ">
+          <h1 className="cursor-pointer hover:text-[#D87D4A] ">Home</h1>
+          <h1 className="cursor-pointer hover:text-[#D87D4A]">HEADPHONES</h1>
+          <h1 className="cursor-pointer hover:text-[#D87D4A]">SPEAKERS</h1>
+          <h1 className="cursor-pointer hover:text-[#D87D4A]">EARPHONES</h1>
+        </nav>
+
+        <button className="cursor-pointer">
+          <img src="./assets/cart.png" alt="" />
+        </button>
+      </div>
+      <div className="w-full h-[1px] bg-[#FFFFFF]"></div>
+    </div>
+  );
+}
