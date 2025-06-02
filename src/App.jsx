e652adeba3249c49989bd0dc48edd6a0a1365c88
nikakakbexs <kakabadzenikanika@gmail.com>
2025-06-02 11:31:21 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-   <div>
-    <Header/>
-    <h1>hello world</h1>
-   </div>
-  )
+    <div>
+      <Header />
+      <h1 className="text-amber-50">hello world</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,20 @@ export default function Header() {
   return (
     <div>
       <div className="w-full bg-[#191919] flex justify-between p-5 ">
-        <div className=" flex h-100% items-center gap-10  ">
+        <div className=" flex h-100% items-center gap-10 ">
           <button className=" cursor-pointer lg:hidden">
             <img src="./assets/menu.svg" alt="" />
           </button>
 
-          <button className="cursor-pointer  ">
+          <button className="cursor-pointer hidden md:block lg:block">
             <img src="./assets/audiophile.svg" alt="" />
           </button>
         </div>
+        <button className="cursor-pointer  lg:hidden md:hidden sm:block">
+          <img src="./assets/audiophile.svg" alt="" />
+        </button>
         <nav className="lg:flex text-[#FFFFFF] gap-8 hidden ">
-          <h1 className="cursor-pointer hover:text-[#D87D4A] ">Home</h1>
+          <h1 className="cursor-pointer hover:text-[#D87D4A]">Home</h1>
           <h1 className="cursor-pointer hover:text-[#D87D4A]">HEADPHONES</h1>
           <h1 className="cursor-pointer hover:text-[#D87D4A]">SPEAKERS</h1>
           <h1 className="cursor-pointer hover:text-[#D87D4A]">EARPHONES</h1>
